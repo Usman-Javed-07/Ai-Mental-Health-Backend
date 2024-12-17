2eb1e639139508c1a0a9e5e8f75bfea28abe8ef8
Usman Javed <usmanjaved0816@gmail.com>
2024-12-17 22:30:27 +0500

    update usermodel

--- config/db.js
+++ config/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error:", err));
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,39 @@
 
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     username: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// // Pre-save hook to hash password
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next(); // Only hash if the password is modified
+//   this.password = await bcrypt.hash(this.password, 10);
+//   next();
+// });
+
+// module.exports = mongoose.model("User", userSchema);
+
+
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,16 +50,10 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      unique: true,
     },
   },
   { timestamps: true }
 );
-
-// Pre-save hook to hash password
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
 
 module.exports = mongoose.model("User", userSchema);
