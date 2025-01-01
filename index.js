3f51d7e26a3499306144e5015495773d21b4f607
Usman Javed <usmanjaved0816@gmail.com>
2025-01-01 22:16:52 +0500

    change port

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use(cors());
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
